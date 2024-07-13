e0bb3880072cdb53a33c4f052d069f8b4ed507ee
Nick Rabone <n.rabone72@aol.com>
2024-07-13 15:13:10 +0100

    Brought in globe images

--- project/src/script.js
+++ project/src/script.js
@@ -12,6 +12,15 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(innerWidth, innerHeight)
 document.body.appendChild(renderer.domElement)
 
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(5, 50, 50),
+    new THREE.MeshBasicMaterial({
+        map: new THREE.TextureLoader().load('./globe.jpg')
+    })
+)
+scene.add(sphere)
+camera.position.z = 10
+
 function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
