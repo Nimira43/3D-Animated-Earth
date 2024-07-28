df3929e98ab2262cb9e70026c5a87e51b6fbe73b
Nick Rabone <n.rabone72@aol.com>
2024-07-28 13:22:47 +0100

    Completed starfield

--- project/src/script.js
+++ project/src/script.js
@@ -56,16 +56,23 @@ scene.add(group)
 
 const starGeometry = new THREE.BufferGeometry()
 const starMaterial = new THREE.PointsMaterial({
-    color: 0xff4500
+    color: 0xffffff
 })
+
 const starVertices = []
 for (let i = 0; i < 10000; i++) {
     const x = (Math.random() - 0.5) * 2000 
     const y = (Math.random() - 0.5) * 2000 
     const z = -Math.random() * 2000
     starVertices.push(x, y, z)
-    console.log(starVertices)
 }
+
+starGeometry.setAttribute('position', 
+    new THREE.Float32BufferAttribute(
+        starVertices, 3
+    )
+)
+
 const stars = new THREE.Points(starGeometry, starMaterial)
 scene.add(stars)
 
